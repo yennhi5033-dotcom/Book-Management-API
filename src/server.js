c8acec4ce92c1a51e0b2a756f1fa4478d7103af1
Nguyen Thi Yen Nhi <nguyentyennhi90@dtu.edu.vn>
2026-09-13 16:17:21 +0700

    feat: add allowed origin for production environment in CORS configuration

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
 
-
+  "https://book-management-api-uzll.onrender.com",
   // Vercel frontend
   "https://book-management-fe-ntyn.vercel.app",
 ];
